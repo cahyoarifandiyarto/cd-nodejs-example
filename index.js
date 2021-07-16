8c307ece38d14ba4cc60ed0f7d8620d6fd488273
Cahyo Arif Andiyarto <cahyoarif.dev@gmail.com>
2021-07-16 17:41:04 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Logos ID Development Testing");
+    res.send("Logos ID Development Testing CD");
 })
 
 const PORT = process.env.PORT || 8080;
