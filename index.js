14f80cc795d03b89f2c97b0e132178419c238dc2
Cahyo Arif Andiyarto <cahyoarif.dev@gmail.com>
2021-07-16 17:02:19 +0700

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Logos ID Development Testing");
+})
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log("Running on port 8080");
+});
